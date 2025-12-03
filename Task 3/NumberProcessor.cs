7f9091dfc5bca4b2c769e806668ded724a77fd58
ritesh969 <rit14.36.14@gmail.com>
2025-12-03 11:38:22 +0545

    Workshop6 Task 3: ProcessNumbers using Func<int,bool> - even and >10 filters

---
+++ Task 3/NumberProcessor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Workshop6
+{
+    public class NumberProcessor
+    {
+        public void ProcessNumbers(int[] numbers, Func<int, bool> condition)
+        {
+            if (numbers == null) return;
+
+            foreach (var n in numbers)
+            {
+                if (condition(n))
+                {
+                    Console.WriteLine(n);
+                }
+            }
+        }
+    }
+}
---
+++ Task 3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Workshop6;
+
+class Program
+{
+    static void Main()
+    {
+        NumberProcessor processor = new NumberProcessor();
+
+        int[] sample = { 3, 4, 10, 12, 15, 22, 7, 8, 30 };
+
+        Console.WriteLine("Only even numbers:");
+        // Now passing a lambda that will returns true for even numbers...
+        processor.ProcessNumbers(sample, n => n % 2 == 0);
+
+        Console.WriteLine("\nOnly numbers greater than 10:");
+        // passing a lambda that will returns true for the numbers > 10...
+        processor.ProcessNumbers(sample, n => n > 10);
+    }
+}
