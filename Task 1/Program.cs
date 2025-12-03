990cf15ad78066c73915cdcf2f0de85ab9403706
ritesh969 <rit14.36.14@gmail.com>
2025-12-03 11:07:35 +0545

    first commit

---
+++ Task 1/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Rectangle r = new Rectangle();
+        r.Length = 10;
+        r.Breadth = 5;
+
+        Console.WriteLine("Area: " + r.GetArea());
+        Console.WriteLine("Perimeter: " + r.GetPerimeter());
+        Console.WriteLine(r.ShowDetails());
+    }
+}
---
+++ Task 1/Rectangle.cs
@@ -0,0 +1,27 @@
+﻿public class Rectangle
+{
+    private double length;
+    private double breadth;
+
+    // Expression-bodied Properties
+    public double Length
+    {
+        get => length;
+        set => length = value;
+    }
+
+    public double Breadth
+    {
+        get => breadth;
+        set => breadth = value;
+    }
+
+    // Expression-bodied Methods
+    public double GetArea() => length * breadth;
+
+    public string ShowDetails() =>
+        $"Length: {length}, Breadth: {breadth}";
+
+    // New Method: Perimeter (Expression-bodied)
+    public double GetPerimeter() => 2 * (length + breadth);
+}
